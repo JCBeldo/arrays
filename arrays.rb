c3be4a5f07e33c525cd0ed00aad3492d95f9ff8e
Julian Beldotti <julian.beldotti@gmail.com>
2023-01-04 15:08:35 -0800

    use methods and add notes

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,17 @@
 names = ["Bob", "Clovis", "Yu", "Stems"]
 ages = [45, 40, 31, 60]
 heights = [176.5, 170.3, 165.4, 162.2]
-are_hungry = [yes, no, no, yes]
+are_hungry = [true, false, false, true]
+
+
+puts names.shift
+puts names.shift
+puts ages.shift
+puts heights.push [186]
+puts ages.unshift [65, 809]
+puts are_hungry.pop
+puts are_hungry
+# shift went to the first in a series
+#push moves one value in
+# unshift adds new elements to the front of the array
+# pop shows leading elements and removes last one
