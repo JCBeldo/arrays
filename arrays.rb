80f19de71c75f39893e0949c9df2a6370159fb0d
JCBeldo <121738722+JCBeldo@users.noreply.github.com>
2023-01-06 11:47:01 -0800

    Update arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -7,15 +7,16 @@ are_hungry = [true, false, false, true]
 puts names.shift
 puts names.shift
 puts ages.shift
-puts heights.push [186]
-puts ages.unshift [65, 809]
+puts heights.push (186) #<-----**
+puts ages.unshift (65, 809) #<-----**
 puts are_hungry.pop
 puts are_hungry
-# shift went to the first in a series
-#push moves one value in
-# unshift adds new elements to the front of the array
-# pop shows leading elements and removes last one
+# shift prints leading element only. removes leading element from array.  #<-----**
+# push adds new arugmented elements to the end of an array.  #<-----**
+# unshift adds new elements to the front of the array. prints array.  #<-----**
+# pop prints last element and removes that element from array.  #<-----**
 
-# index positon or bracket position tells ruby how to put elements in arrays based on positon in the array. The first position is 0. 
+# index positon or bracket position is the position of elements in an array starting at position 0.  #<-----**
+# ex: [0, 1, 2, 3] for ["bob", "larry", "bill", "tom"] bob=0, larry =1 etc..  #<-----**
 puts names.zip [ages, heights]
 # zip takes arrays and nests them and orders them.
